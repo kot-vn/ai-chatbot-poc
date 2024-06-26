b0c2d93ec629b4e09756a21e510dc4cdc240fcf7
behemoth-kienvt <kienvt@behemoth.vn>
2024-06-26 13:36:53 +0700

    add knowledge table

--- knowledge/models.py
+++ knowledge/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Knowledge(models.Model):
+    account_id = models.IntegerField()
+    url = models.CharField(max_length=255)
+    collection_id = models.UUIDField(primary_key=True)
+
+    class Meta:
+        db_table = "knowledge"
+        managed = False
