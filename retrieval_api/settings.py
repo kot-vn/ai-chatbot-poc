8c555f7115bb053608be090f0fb12b5dd0da1b81
behemoth-kienvt <kienvt@behemoth.vn>
2024-06-26 13:40:36 +0700

    add env example

--- retrieval_api/settings.py
+++ retrieval_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sch4(*05=c_*to*av83n1age4+rf-x5kl(z75^55r=5hs2(&se"
+SECRET_KEY = config("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default="False") != "False"
